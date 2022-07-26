24915da817c00029b8f7edb9a3a6cbdde3cc1e39
Jedidiah Foster <jedidiah.foster@gmail.com>
2022-07-26 07:34:28 -0700

    Updated examples for new directory structure

---
+++ examples/basic_usage_by_all/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  default_tags {
+    tags = {
+      environment = "dev"
+      terraform   = "true"
+    }
+  }
+}
+
+module "shield-all" {
+  source  = "so1omon563/shield-advanced/aws//modules/group_by_all"
+  version = "2.0.0" # Replace with appropriate version
+
+  tags = {
+    example = "true"
+  }
+}
+
+output "shield-all" {
+  value = module.shield-all
+}
