5da5da5f77a4cc07c8d60e53105cd308e7bb6868
Jedidiah Foster <jedidiah.foster@gmail.com>
2026-06-27 17:25:29 -0700

    Add Terraform tests and CI for Shield Advanced (#17)
    test: add terraform checks for shield

--- test/basic-usage/basic-usage.tf
+++ test/basic-usage/basic-usage.tf
@@ -1,4 +1,7 @@
 provider "aws" {
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+
   default_tags {
     tags = {
       environment = "dev"
@@ -7,23 +10,13 @@ provider "aws" {
   }
 }
 
-data "aws_availability_zones" "available" {}
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-
-# Create an Elastic IP to be protected
-resource "aws_eip" "example" {
-  #checkov:skip=CKV2_AWS_19:EIP is for example only
-  domain = "vpc"
-}
-
-# Protect the EIP
+# Protect an example resource ARN.
 module "shield" {
   source = "../.."
 
   # Pass in the name you wish to use for the resource, and the ARN of the resource to be protected.
   name_resource_arn_map = {
-    "basic_testing_resource" = "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:eip-allocation/${aws_eip.example.id}"
+    "basic_testing_resource" = "arn:aws:ec2:us-east-1:123456789012:eip-allocation/eipalloc-example"
   }
   tags = {
     example = "true"
