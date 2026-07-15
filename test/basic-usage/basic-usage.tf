7adeccabe5de928323ef11e7091d014df8441180
Jedidiah Foster <jedidiah.foster@gmail.com>
2026-07-14 20:57:18 -0700

    Add standalone Terraform metadata (#21)
    * Add standalone Terraform metadata (SO1-117)
    
    * Require AWS provider v5 in EIP examples

--- test/basic-usage/basic-usage.tf
+++ test/basic-usage/basic-usage.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
+
 provider "aws" {
   skip_credentials_validation = true
   skip_requesting_account_id  = true
